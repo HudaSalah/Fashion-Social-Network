cea441ea7808d918ed82e3afb0facad8d432ea5d
HudaSalah <hudasalah29@gmail.com>
2018-12-15 23:54:26 +0200

    upload proj environment

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import * as $ from 'jquery'; // to use jquery
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
